5ae6d2eedad71ef2e4d8e1d8ce3e19aab232d337
Jurijus Pacalovas <pj.midgard@yahoo.com>
2023-09-08 19:03:46 +0100

    Update Portal-7.py

--- Portal-7.py
+++ Portal-7.py
@@ -1,374 +1,250 @@
 import os  
 
-
 from time import time
-
 
 import binascii
 
-
 import math
-
 
 import os.path
 
-
 namez = input("program: c and old program: e")
-
 
 #@Author Jurijus pacalovas
 
-
 #class03917606322JurijusPacalovasmade.pdf compression:03917606322JurijusPacalovasmade.pdf65536
-
 
 class compression:
 
-
     def cryptograpy_compression(self):
-
 
                 
 
-
                 self.name = "Created: Jurijus pacalovas Price Portal 1000 000 000 Euro cost Date: 26/07/2021 17:11 Deep 14.5 ERA"
-
 
                 if namez=="c":
 
-
                     corridors=0
-
 
                     cor=7
 
-
                     name = input("What is name of file? ")
-
 
                     namea="file.Secret"
 
-
                     namem=""
-
 
                     namema="?"
 
 
 
-
-
                     e5=0
-
 
                     e6=0
 
-
                     e7=255
-
 
                     e8=0
                     e9=0
 
 
-
                     sda3=""
 
-
                     sda4=""
-
 
                     sda5=""
 
 
 
-
-
                    
 
-
                  
-
 
                     assxw=0
 
-
                     blockw=5
-
 
                     blockw1=4
 
-
                     nameas=name
 
-
                     nac=len(nameas)
-
 
                     nameas=name+".bin"
 
-
                     
-
 
                     nac=len(nameas)
 
-
                     
-
 
                     countraz=0
 
-
                     cvf=2
-
 
                     cvf1=0
 
-
                     s=""
-
 
                     e2=0
 
-
                     e3=2
-
 
                     e4=""
 
-
                     c=2
-
 
                     sw=2
 
-
                     elw=0
-
 
                  
 
-
                     sda3=""
-
 
                     sda2=""
 
 
 
-
-
                     sscvf=0
 
-
                     
-
 
                     qqqqwzl=0
 
 
 
-
-
                     block=1
-
 
                     r=""
 
 
 
-
-
                     x=0
-
 
                     x1=0
 
-
                     x2=0
-
 
                     x = time()
 
 
 
-
-
                     with open(nameas, "w") as f4:
-
 
                             f4.write(s)
 
-
                     with open(nameas, "a") as f3:
 
-
                             f3.write(s)
-
 
                     with open(name, "rb") as binary_file:
 
 
 
-
-
                        # Read the whole file at once
-
 
                         data = binary_file.read()
 
-
                         s=str(data)
 
-
                        
-
 
                         lenf1=len(data)
 
-
                         lenf5=len(data)
 
-
                         
-
 
                         assx=0
 
-
                         qqqwz=0
-
 
                         
 
-
                        
-
 
                         while assx<10:
 
-
                        
 
-
                             aas1=0
-
 
                             a1=0
 
 
 
-
-
                             cvf=cvf+1
 
-
                             
-
 
                             countraz=countraz+1
 
 
 
-
-
                             with open(nameas, "ab") as f2:
-
 
                                 if countraz==1:
 
-
                                     sda=bin(int(binascii.hexlify(data),16))[2:]
-
 
                                     lenf=len(sda)
 
-
                                     lenf1=len(data)
-
 
                                 
 
-
                                     xc=(lenf1*8)-lenf
-
 
                                     z=0
 
-
                                     if xc!=0:
-
 
                                         while z<xc:
 
-
                                             sda="0"+sda
-
 
                                             z=z+1
 
-
                                             
-
 
                                     sda=sda+sda2
 
 
 
-
-
                                     if countraz==1:
-
 
                                         sda2=sda
 
-
                             
-
 
                                     n = int(sda2, 2)
 
-
                                 
-
 
                                     qqwslenf=len(sda2)
 
-
                                     qqwslenf=(qqwslenf/8)*2
-
 
                                     qqwslenf=str(qqwslenf)
 
-
                                     qqwslenf="%0"+qqwslenf+"x"
-
 
                              
 
-
                                     jl=binascii.unhexlify(qqwslenf % n)
-
 
                                     sssssw=len(jl)
 
-
                                     
-
 
                                     data=jl
 
-
                                     qqqwz=qqqwz+1
-
 
                                    
 
-
                                     if countraz==1:
-
-
 
 
 
@@ -376,41 +252,27 @@ class compression:
 
 
 
-
-
                                     sda=bin(int(binascii.hexlify(data),16))[2:]
-
 
                                     lenf=len(sda)
 
 
 
-
-
                                     lenf1=len(data)
-
 
                                 
 
-
                                     xc=(lenf1*8)-lenf
-
 
                                     z=0
 
-
                                     if xc!=0:
-
 
                                         while z<xc:
 
-
                                             sda="0"+sda
 
-
                                             z=z+1
-
-
 
 
 
@@ -418,29 +280,19 @@ class compression:
 
 
 
-
-
                                     lenf3=len(sda2)
 
-
                                 lenf2=len(sda2)
-
 
                                 c=1
 
 
 
-
-
                                
-
 
              
 
-
                                 if c==1:
-
-
 
 
 
@@ -448,76 +300,51 @@ class compression:
 
 
 
-
-
                                     while block<lenf2:
-
-
 
 
 
                            
 
-
                                     
-
-
 
 
 
                                         e4=sda2[block:block+8]
 
-
                                         
 
-
                                         e5=int(e4,2)
-
 
                                         if e5==e6:
 
 
 
-
-
                                             
-
 
                                             sda3=format(e7,"08b")
 
-
                                         elif e5==e7:
 
-
                                            
-
 
                                             sda3=format(e6,"08b")
 
 
 
-
-
                                         else:
 
-
                                             
-
 
                                             sda3=format(e5,"08b")
 
-
                                         sda4+=sda3
-
 
                                             
 
-
                                         block+=8                                    
 
-
                                     
-
 
                                     e6+=10
                                     e7-=10
@@ -532,19 +359,13 @@ class compression:
                                     sda4=""
                                     if cvf1==25:
                                                     
-
-
-                                    
-
+                                                    
 
                                     
 
+                                    
 
                                       
-
-
-
-
 
 
 
@@ -554,430 +375,288 @@ class compression:
 
                                   
 
-
                                         
-
 
                                         n = int(sda2, 2)
 
-
                                         
-
 
                                         qqwslenf=len(sda2)
 
-
                                         qqwslenf=(qqwslenf/8)*2
-
 
                                         qqwslenf=str(qqwslenf)
 
-
                                         qqwslenf="%0"+qqwslenf+"x"
-
 
                                      
 
-
                                         jl=binascii.unhexlify(qqwslenf % n)
-
 
                                         sssssw=len(jl)
 
-
                                         data=jl
-
 
                                         qqqwz=qqqwz+1
 
-
                                         szxzzza=""
-
 
                                         szxzs=""
 
-
                                     
-
 
                                         assxw=assxw+1
 
-
                                         if assxw==1:
-
 
                                                 assx=10
 
-
                                                 if assx==10:
-
 
                                                   
 
-
                                                     f2.write(jl)
-
 
                                                     x2 = time()
 
-
                                                     x3=x2-x
-
 
                                                     return print(x3)
 
-
                                         
 
-
                                         
-
 
                                       
 
-
                                             
 
-
                                     
-
-
 
 
 
     def cryptograpy_unpack(self):                                 
 
-
                  if namez=="e":
-
 
                     corridors=0
 
-
                     cor=7
-
 
                     name = input("What is name of file? ")
 
-
                     
-
 
                     namea="file.Secret"
 
-
                     namem=""
-
 
                     namema="?"
 
 
-                    
-                    e5=0
 
+                    e5=0
 
                     e6=0
 
-
                     e7=255
-
-
                     e8=0
                     e9=0
 
 
-
                     sda3=""
 
-
                     sda4=""
-
 
                     sda5=""
 
 
 
-
-
                  
-
 
                     assxw=0
 
-
                     blockw=5
-
 
                     blockw1=4
 
-
                     nameas=name
 
-
                     nac=len(nameas)
-
 
                     nameas=name[:nac-4]
 
-
                     nac=len(nameas)
-
 
                     
 
-
                     countraz=0
-
 
                     cvf=2
 
-
                     cvf1=0
-
 
                     s=""
 
-
                     e2=0
-
 
                     e3=2
 
-
                     e4=""
-
 
                     c=2
 
-
                     sw=2
-
 
                     elw=0
 
-
                     cr=0
-
 
                  
 
-
                     sda3=""
-
 
                     sda2=""
 
 
 
-
-
                     sscvf=0
 
-
                     
-
 
                     qqqqwzl=0
 
 
 
-
-
                     block=1
-
 
                     cvf1=0
 
 
 
-
-
                     x=0
-
 
                     x1=0
 
-
                     x2=0
-
 
                     x = time()
 
 
 
-
-
                     with open(nameas, "w") as f4:
-
 
                             f4.write(s)
 
-
                     with open(nameas, "a") as f3:
 
-
                             f3.write(s)
-
 
                     with open(name, "rb") as binary_file:
 
 
 
-
-
                         # Read the whole file at once
-
 
                         data = binary_file.read()
 
-
                         s=str(data)
 
-
                         
-
 
                         lenf1=len(data)
 
-
                         lenf5=len(data)
-
 
                         
 
-
                         assx=0
-
 
                         qqqwz=0
 
-
                        
-
 
                         while assx<10:
 
-
                        
 
-
                             aas1=0
-
 
                             a1=0
 
 
 
-
-
                             cvf=cvf+1
 
-
                             
-
 
                             countraz=countraz+1
 
 
 
-
-
                             with open(nameas, "ab") as f2:
-
 
                                 if countraz==1:
 
-
                                     sda=bin(int(binascii.hexlify(data),16))[2:]
-
 
                                     lenf=len(sda)
 
-
                                     lenf1=len(data)
-
 
                                 
 
-
                                     xc=(lenf1*8)-lenf
-
 
                                     z=0
 
-
                                     if xc!=0:
-
 
                                         while z<xc:
 
-
                                             sda="0"+sda
-
 
                                             z=z+1
 
-
                                             
-
 
                                     sda=sda+sda2
 
 
 
-
-
                                     if countraz==1:
-
 
                                         sda2=sda
 
-
                             
-
 
                                     n = int(sda2, 2)
 
-
                                 
-
 
                                     qqwslenf=len(sda2)
 
-
                                     qqwslenf=(qqwslenf/8)*2
-
 
                                     qqwslenf=str(qqwslenf)
 
-
                                     qqwslenf="%0"+qqwslenf+"x"
-
 
                              
 
-
                                     jl=binascii.unhexlify(qqwslenf % n)
-
 
                                     sssssw=len(jl)
 
-
                                     
-
 
                                     data=jl
 
-
                                     qqqwz=qqqwz+1
-
 
                                    
 
-
                                     if countraz==1:
-
-
 
 
 
@@ -985,41 +664,27 @@ class compression:
 
 
 
-
-
                                     sda=bin(int(binascii.hexlify(data),16))[2:]
-
 
                                     lenf=len(sda)
 
 
 
-
-
                                     lenf1=len(data)
-
 
                                 
 
-
                                     xc=(lenf1*8)-lenf
-
 
                                     z=0
 
-
                                     if xc!=0:
-
 
                                         while z<xc:
 
-
                                             sda="0"+sda
 
-
                                             z=z+1
-
-
 
 
 
@@ -1027,17 +692,11 @@ class compression:
 
 
 
-
-
                                     lenf3=len(sda2)
-
 
                                 lenf2=len(sda2)
 
-
                                 c=1
-
-
 
 
 
@@ -1045,11 +704,7 @@ class compression:
 
 
 
-
-
                                     block=0
-
-
 
 
 
@@ -1057,73 +712,47 @@ class compression:
 
 
 
-
-
                            
 
-
                                     
-
-
 
 
 
                                         e4=sda2[block:block+8]
 
-
                                         
 
-
                                         e5=int(e4,2)
-
 
                                         if e5==e6:
 
 
 
-
-
                                             
-
 
                                             sda3=format(e7,"08b")
 
-
                                         elif e5==e7:
 
-
                                            
-
 
                                             sda3=format(e6,"08b")
 
 
 
-
-
                                         else:
 
-
                                             
-
 
                                             sda3=format(e5,"08b")
 
-
                                         sda4+=sda3
-
 
                                             
 
-
                                         block+=8                                    
 
-
                                     
-
-
-                                    
-
 
                                     e6+=10
                                     e7-=10
@@ -1137,96 +766,66 @@ class compression:
                                     sda2=sda4
                                     sda4=""
                                     if cvf1==25:
+                                                    
                                                    
 
-
                                         
 
-
                                         
-
 
                                         n = int(sda2, 2)
 
-
                                         
-
 
                                         qqwslenf=len(sda2)
 
-
                                         qqwslenf=(qqwslenf/8)*2
-
 
                                         qqwslenf=str(qqwslenf)
 
-
                                         qqwslenf="%0"+qqwslenf+"x"
-
 
                                      
 
-
                                         jl=binascii.unhexlify(qqwslenf % n)
-
 
                                         sssssw=len(jl)
 
-
                                         data=jl
-
 
                                         qqqwz=qqqwz+1
 
-
                                         szxzzza=""
-
 
                                         szxzs=""
 
-
                                     
-
 
                                         assxw=assxw+1
 
-
                                         if assxw==1:
-
 
                                                 assx=10
 
-
                                                 if assx==10:
-
 
                                                 
 
-
                                                     f2.write(jl)
-
 
                                                     x2 = time()
 
-
                                                     x3=x2-x
 
-
                                                     return print(x3)
-
 
                      
 
 
 
-
-
                                 
 
-
                
-
-
 
 
 
@@ -1234,18 +833,12 @@ d=compression()
 
 
 
-
-
 xw=d.cryptograpy_compression()
-
 
 print(xw)
 
 
 
-
-
 xw1=d.cryptograpy_unpack()
-
 
 print(xw1)
