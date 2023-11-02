277c244c576b7b269088e7619fc1c74362fb4016
Jurijus Pacalovas <pj.midgard@yahoo.com>
2023-11-02 19:39:00 +0000

    Add files via upload

--- Dark_Hole-1.0.0.0.py
+++ Dark_Hole-1.0.0.0.py
@@ -160,8 +160,8 @@ class compression:
                                                 X1=1
                                                 X2=1
                                             University_file=format(University,C11)
-                                            Divide=int(University_file[0:(X2*8)],2)
-                                            Times_12=int(University_file[(X2*8):(X2*8)+8],2)
+                                            
+                                            Times_12=int(University_file[(X2*0):(X2*8)+8],2)
                                             Multiply=int(University_file[(X2*8)+8:(X2*8)+16],2)
                                             Add_Numbers=int(University_file[(X2*8)+16:(X2*8)+24],2)
                                             SQUEAR_OF_ROOT=int(University_file[(X2*8)+24:(X2*8)+32],2)
